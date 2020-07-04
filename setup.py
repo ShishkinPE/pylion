b4ddc4a9abc03f86389bccb342c1b814bb331174
dimitris <dtrypogiorgos@gmail.com>
2020-07-04 13:15:30 +0200

    Bump version: 0.3.6 → 0.3.7

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requirements = [
 
 setup(
     name='pylion',
-    version='0.3.6',
+    version='0.3.7',
     description="A LAMMPS wrapper for molecular dynamics simulations of trapped ions.",
     long_description=readme,
     author="Dimitris Trypogeorgos",
