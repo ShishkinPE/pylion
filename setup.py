c6f0cc1112f8128eed00581691ffeb3ff7a16bd0
dimitris <dtrypogiorgos@gmail.com>
2020-07-06 13:38:05 +0200

    Bump version: 0.4.0 → 0.5.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ short_description = (
 
 setup(
     name='pylion',
-    version='0.4.0',
+    version='0.5.0',
     description=short_description,
     long_description=readme,
     author="Dimitris Trypogeorgos",
